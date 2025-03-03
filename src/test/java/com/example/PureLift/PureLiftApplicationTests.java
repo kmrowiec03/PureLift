76939113d268ad02b60ae8e6d9bac736d3ee3ae3
Kacper <kacper3mrowiec@gmail.com>
2025-03-03 22:59:43 +0100

    add project to github

---
+++ src/test/java/com/example/PureLift/PureLiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PureLift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureLiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
